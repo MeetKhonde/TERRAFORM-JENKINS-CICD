aa4d4c44e015503ce0584fa4699af96bc07ee4c8
MeetKhonde <meetkhonde467@gmail.com>
2025-06-12 17:03:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Khonde"
+  default     = "CI-CD"
 }
 variable "instance_type" {
   description = "instance type for ec2"
